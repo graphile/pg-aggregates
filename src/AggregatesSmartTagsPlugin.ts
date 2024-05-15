a839fd18b1f85671867d79cc1bb41ab9dcd68a62
Benjie Gillam <benjie@jemjie.com>
2024-05-15 12:03:04 +0100

    Fix bug with porting smart tags over

--- src/AggregatesSmartTagsPlugin.ts
+++ src/AggregatesSmartTagsPlugin.ts
@@ -55,13 +55,13 @@ function processTags(
     case "on":
       addBehaviorToTags(
         tags,
-        "+aggregates +aggregates:filter +aggregates:order +aggregate +aggregate:filterBy +aggregate:orderBy"
+        "+aggregates +aggregates:filterBy +aggregates:orderBy +aggregate +aggregate:filterBy +aggregate:orderBy"
       );
       break;
     case "off":
       addBehaviorToTags(
         tags,
-        "-aggregates -aggregates:filter -aggregates:order -aggregate -aggregate:filterBy -aggregate:orderBy"
+        "-aggregates -aggregates:filterBy -aggregates:orderBy -aggregate -aggregate:filterBy -aggregate:orderBy"
       );
       break;
   }
